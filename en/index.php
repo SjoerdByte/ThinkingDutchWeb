095415113e92223531a0e1c5f336ffa6d1e5d3c9
jimdeg <57574341+jimdeg@users.noreply.github.com>
2022-10-13 11:44:49 +0200

    lang base

---
+++ en/index.php
@@ -0,0 +1,42 @@
+<html lang="nl"><head>
+    <title>ThinkingDutch | Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand" href="/">
+                <img class="navbar-image" src="../assets/img/logo_transparant.png"
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="navbar" style="">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<main class="container">
+    <div class="row mt-5">
+
+    </div>
+</main>
+<!--<footer class="footer sticky">-->
+<!--    <div class="container">-->
+<!--        <span>© <b>ThinkingDutch</b> 2022</span>-->
+<!--    </div>-->
+<!--</footer>-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
