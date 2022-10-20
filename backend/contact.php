853d17ce0ebe54f23f244d56e47229b29185d923
jimdeg <57574341+jimdeg@users.noreply.github.com>
2022-10-20 14:47:35 +0200

    email change

--- backend/contact.php
+++ backend/contact.php
@@ -15,7 +15,7 @@ $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $email . "\r\n";
 
-if(mail('d295570@edu.curio.nl', 'Contact formulier', $body, $headers)) {
+if(mail('info@thinkingdutch.com', 'Contact formulier', $body, $headers)) {
     header('Location: ' . $_SERVER['HTTP_REFERER'] .'?status=success');
 } else {
     header('Location: ' . $_SERVER['HTTP_REFERER'] .'?status=error');
