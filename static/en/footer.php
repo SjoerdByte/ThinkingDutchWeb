9734a020f92fe5ad88b889887cb351ed63ad12ca
jimdeg <57574341+jimdeg@users.noreply.github.com>
2022-10-21 09:52:01 +0200

    footer fix

--- static/en/footer.php
+++ static/en/footer.php
@@ -1,38 +1,40 @@
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer class="bg-light text-muted">
     <div class="py-3">
-        <div class="container text-center text-md-start mt-5">
+        <div class="container mt-5">
             <div class="row mt-3">
-                <div class="col-12 col-lg-4 mx-auto mb-4">
-                    <h6 class="mb-4">
-                        <img class="footer-logo" alt="ThinkingDutch logo" src="../assets/img/logo_transparant.png">
-                    </h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et rhoncus neque, vitae rutrum ligula.
-                    </p>
+                <div class="col-12 col-lg-5 mx-auto mb-4">
+                    <div class="col-10 text-center">
+                        <h6 class="mb-4">
+                            <img class="footer-logo" alt="ThinkingDutch logo" src="../assets/img/logo_transparant.png">
+                        </h6>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et rhoncus neque, vitae rutrum ligula.
+                        </p>
+                    </div>
                 </div>
-                <div class="col-12 col-lg-4 mx-auto mb-4">
+                <div class="col-12 col-lg-2 mb-4 d-none d-lg-block">
                     <h6 class="text-uppercase fw-bold mb-4">
                         Links
                     </h6>
                     <p>
-                        <a href="/en/" class="text-reset">Home</a>
+                        <a href="/nl/" class="text-reset">Home</a>
                     </p>
                     <p>
-                        <a href="/en/products.php" class="text-reset">Products</a>
+                        <a href="/nl/products.php" class="text-reset">Products</a>
                     </p>
                     <p>
-                        <a href="/en/pricing.php" class="text-reset">Pricing</a>
+                        <a href="/nl/pricing.php" class="text-reset">Pricing</a>
                     </p>
                     <p>
-                        <a href="/en/contact.php" class="text-reset">Contact</a>
+                        <a href="/nl/contact.php" class="text-reset">Contact</a>
                     </p>
                 </div>
-                <div class="col-12 col-lg-4 mx-auto mb-md-0 mb-4">
+                <div class="col-12 col-lg-4 mx-auto mb-md-0 mb-4 text-xs-center">
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
                     <p>
                         ThinkingDutch<br>
                         Theodora Haverstraat 18<br>
-                        5122BD Rijen<br>
+                        5122BD Gilze-Rijen<br>
                         The Netherlands
                     </p>
                     <p>
@@ -43,7 +45,7 @@
         </div>
     </div>
     <div class="p-4 d-flex justify-content-between" style="background-color: rgba(0, 0, 0, 0.05);">
-        <small>
+        <small class="d-none d-lg-block">
             CoC: <b>18083274</b> | VAT: <b>NL001676141B50</b> | Bank details: <b>NL93 RABO 0125 4250 07</b>
         </small>
         <div class="list-unstyled d-flex">
@@ -51,8 +53,3 @@
         </div>
     </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/javascript.js"></script>
-</body>
-</html>
--- static/nl/footer.php
+++ static/nl/footer.php
@@ -1,16 +1,18 @@
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer class="bg-light text-muted">
     <div class="py-3">
-        <div class="container text-center text-md-start mt-5">
+        <div class="container mt-5">
             <div class="row mt-3">
-                <div class="col-12 col-lg-4 mx-auto mb-4">
-                    <h6 class="mb-4">
-                        <img class="footer-logo" alt="ThinkingDutch logo" src="../assets/img/logo_transparant.png">
-                    </h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et rhoncus neque, vitae rutrum ligula.
-                    </p>
+                <div class="col-12 col-lg-5 mx-auto mb-4">
+                    <div class="col-10 text-center">
+                        <h6 class="mb-4">
+                            <img class="footer-logo" alt="ThinkingDutch logo" src="../assets/img/logo_transparant.png">
+                        </h6>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et rhoncus neque, vitae rutrum ligula.
+                        </p>
+                    </div>
                 </div>
-                <div class="col-12 col-lg-4 mx-auto mb-4">
+                <div class="col-12 col-lg-2 mb-4 d-none d-lg-block">
                     <h6 class="text-uppercase fw-bold mb-4">
                         Links
                     </h6>
@@ -27,12 +29,12 @@
                         <a href="/nl/contact.php" class="text-reset">Contact</a>
                     </p>
                 </div>
-                <div class="col-12 col-lg-4 mx-auto mb-md-0 mb-4">
+                <div class="col-12 col-lg-4 mx-auto mb-md-0 mb-4 text-xs-center">
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
                     <p>
                         ThinkingDutch<br>
                         Theodora Haverstraat 18<br>
-                        5122BD Rijen<br>
+                        5122BD Gilze-Rijen<br>
                         Nederland
                     </p>
                     <p>
@@ -43,7 +45,7 @@
         </div>
     </div>
     <div class="p-4 d-flex justify-content-between" style="background-color: rgba(0, 0, 0, 0.05);">
-        <small>
+        <small class="d-none d-lg-block">
             KvK: <b>18083274</b> | BTW: <b>NL001676141B50</b> | Rabobank: <b>NL93 RABO 0125 4250 07</b>
         </small>
         <div class="list-unstyled d-flex">
