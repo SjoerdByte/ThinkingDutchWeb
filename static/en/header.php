639241df0ad6e8deff4673859ae9131776f36649
jimdeg <57574341+jimdeg@users.noreply.github.com>
2022-11-04 09:13:45 +0100

    language switcher fix

--- static/en/header.php
+++ static/en/header.php
@@ -8,15 +8,15 @@
     <title>ThinkingDutch | <?php echo ucfirst($currentPage) ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="/assets/img/favicon.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>
 <header>
     <div id="navbar" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 box-shadow navbar-fixed">
         <a href="/en" class="my-0 mr-md-auto">
-            <img class="navbar-image" src="../assets/img/logo_transparant.png">
+            <img class="navbar-image" src="/assets/img/logo_transparant.png">
         </a>
         <nav class="mb-3 mt-3 mb-md-0 mt-md-0">
             <a class="p-2 p-md-3 nav-item <?php if($currentPage == 'home') echo 'nav-active' ?>" href="/en">Home</a>
@@ -26,16 +26,16 @@
         </nav>
         <div class="dropdown nav-item">
             <a class="dropdown dropdown-toggle language-button" id="changeLanguage" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="rounded-sm language-flag" alt="UK flag" src="../assets/img/languages/uk.png">
+                <img class="rounded-sm language-flag" alt="UK flag" src="/assets/img/languages/uk.png">
                 <span>English</span>
             </a>
             <div class="dropdown-menu" aria-labelledby="changeLanguage">
                 <a class="dropdown-item" href="/nl/<?php echo $switchPageUrl; ?>">
-                    <img class="rounded-sm language-flag" alt="Dutch flag" src="../assets/img/languages/nl.png">
+                    <img class="rounded-sm language-flag" alt="Dutch flag" src="/assets/img/languages/nl.png">
                     Dutch
                 </a>
                 <a class="dropdown-item" href="/en/<?php echo $switchPageUrl; ?>">
-                    <img class="rounded-sm language-flag" alt="UK flag" src="../assets/img/languages/uk.png">
+                    <img class="rounded-sm language-flag" alt="UK flag" src="/assets/img/languages/uk.png">
                     English
                 </a>
             </div>
--- static/nl/header.php
+++ static/nl/header.php
@@ -8,15 +8,15 @@
     <title>ThinkingDutch | <?php echo ucfirst($currentPage); ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="/assets/img/favicon.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>
 <header>
     <div id="navbar" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 box-shadow navbar-fixed">
         <a href="/nl" class="my-0 mr-md-auto">
-            <img class="navbar-image" src="../../assets/img/logo_transparant.png">
+            <img class="navbar-image" src="/assets/img/logo_transparant.png">
         </a>
         <nav class="mb-3 mt-3 mb-md-0 mt-md-0">
             <a class="p-2 p-md-3 nav-item <?php if($currentPage == 'home') echo 'nav-active' ?>" href="/nl">Home</a>
@@ -26,16 +26,16 @@
         </nav>
         <div class="dropdown nav-item">
             <a class="dropdown dropdown-toggle language-button" id="changeLanguage" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="rounded-sm language-flag" alt="Dutch flag" src="../../assets/img/languages/nl.png">
+                <img class="rounded-sm language-flag" alt="Dutch flag" src="/assets/img/languages/nl.png">
                 <span>Nederlands</span>
             </a>
             <div class="dropdown-menu" aria-labelledby="changeLanguage">
                 <a class="dropdown-item" href="/en/<?php echo $switchPageUrl; ?>">
-                    <img class="rounded-sm language-flag" alt="UK flag" src="../../assets/img/languages/uk.png">
+                    <img class="rounded-sm language-flag" alt="UK flag" src="/assets/img/languages/uk.png">
                     Engels
                 </a>
                 <a class="dropdown-item" href="/nl/<?php echo $switchPageUrl; ?>">
-                    <img class="rounded-sm language-flag" alt="Dutch flag" src="../../assets/img/languages/nl.png">
+                    <img class="rounded-sm language-flag" alt="Dutch flag" src="/assets/img/languages/nl.png">
                     Nederlands
                 </a>
             </div>
