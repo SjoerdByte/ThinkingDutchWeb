f51453363770faa670c32abf12771b354aa2e509
Sjoerd <sjoerd.pelzer@gmail.com>
2025-05-22 14:32:25 +0200

    Update index

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+if (!empty($_SERVER['QUERY_STRING'])) {
+    header("Location: https://thinkingdutch.com/nl/pricing", true, 301);
+    exit();
+}
 
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 $lang = in_array($lang, ['en', 'nl']) ? $lang : 'en';
 
 header("Location: /" . $lang);
+
